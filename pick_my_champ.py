a333eed79424448a19cf8db0413d37ad87d80edd
Ultraempoleon <44034263+Ultraempoleon@users.noreply.github.com>
2022-07-02 22:55:30 -0700

    Discovered a huge problem
    There's a big problem I discovered, the program can't change more than 1 line in a single run
    
    So instead Im going to combine lines 2 and 3 into a single line

--- pick_my_champ.py
+++ pick_my_champ.py
@@ -1,4 +1,5 @@
 import random
+from sqlite3 import connect
 random.seed()
 #So the idea here is that I got bored trying to decide what champion to
 #play so I made this for fun
@@ -46,10 +47,12 @@ def concat_list_into_word(champ_list):
     return word
 
 
+#this function selects a champion from 
 def select_champion(champ_list):
     list_size = len(champ_list)
     #picks a random number from 0 - #of champs in list
     number = random.randrange(0,len(champ_list))
+    print()
     if (list_size-1 == 0):
         champ = champ_list[0]
         champ_list.pop(0)
@@ -70,6 +73,39 @@ def select_champion(champ_list):
     return "Error occurred, no champion slected"
 
 
+#the idea here is to change the line of the file seperately 
+def changelines1(champions):
+    print("\nim here")
+    myfile = open("champs.txt") #opens file
+    file_content = myfile.readlines() #seperates lines so that each line can be read individually
+
+    try:
+        level = int(file_content[1])
+    except:
+        print("there is no line2?")
+
+    print("level: {}".format(level))
+    print("content[1]: {}".format(file_content[1]))
+
+    if (level+1) > len(champions):
+        level = 0
+    else:
+        level += 1
+
+
+    file_content[1] = str(level)
+
+    
+    print("level: {}".format(level))
+    print("content[1]: {}".format(file_content[1]))
+    
+
+    with open("champs.txt", 'w') as file_again:
+        file_again.writelines(file_content)
+
+    print("\nDone with function")
+
+
 def main():
     champions = [] #all the champions on the list
     current_level_champions = [] #champions at that level
@@ -86,37 +122,43 @@ def main():
     #checks if the third line exists
     try:
         #if it does exists, adds that list of champions into the list
-        if (content[2] != "\n"):
+        if (content[2] != "empty "):
+            print("butts!!!!")
             serperate_into_words(content[2], current_level_champions) #put the champions on the second list
+
 
         #if there is not a list (the assumption is that the previous level was finished)
         #it will create a new pool adding one more champion to it (i.e. the next level)
-        else:
+        elif (content[2] == "empty "):
+            changelines1(champions)
+            print(content[2])
             print("\nbutts")
             #if the level would exceed the maximum number of champions
             #level is set to 0; else level is raised by 1
-            if (level+1) > len(champions):
-                level = 0
-            else:
-                level += 1
+
+            # if (level+1) > len(champions):
+            #     level = 0
+            # else:
+            #     level += 1
 
             generate_list(level, current_level_champions, champions)
             champion_list_string = concat_list_into_word(current_level_champions)
             content[2] = champion_list_string
-            content[1] = str(level)
+            #content[1] = str(level)
 
     except IndexError:
         print("\nUhhh there is no line 3, write something on that line or indent on it")
         
-        if (level+1) > len(champions):
-            level = 0
-        else:
-            level += 1
+        # if (level+1) > len(champions):
+        #     level = 0
+        # else:
+        #     level += 1
         generate_list(level, current_level_champions, champions)
         champion_list_string = concat_list_into_word(current_level_champions)
         
+
         content[2] = champion_list_string
-        content[1] = str(level)
+        #content[1] = str(level)
 
     print("Champions to pick from")
     for champs in current_level_champions:
@@ -125,14 +167,15 @@ def main():
     
     champion_selected = select_champion(current_level_champions)
     print("\nChampion Selected: {}\n".format(champion_selected))
+    
     if (len(current_level_champions) == 0):
-        print("\nButt")
-        content[2] = "\n"
+        content[2] = "empty "
+
     else:
         champion_list_string = concat_list_into_word(current_level_champions)
         content[2] = champion_list_string
 
-
+    print(content[2])
     with open("champs.txt", 'w') as file2:
         file2.writelines( content )
 
